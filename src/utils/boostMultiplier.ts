f85a73182b397209cc5e2d6f4e5a25dcb2677711
Ismael <iaarabu@gmail.com>
2023-03-04 04:08:37 +0000

    fix typo and nultiplier boost

--- src/utils/boostMultiplier.ts
+++ src/utils/boostMultiplier.ts
@@ -1,11 +1,11 @@
 export const getBoostMultiplier = (boost: string) => {
   const numericBoost = +boost;
   if (numericBoost === -6) return 0.25;
-  if (numericBoost === -5) return 0.29;
-  if (numericBoost === -4) return 0.33;
+  if (numericBoost === -5) return 7 / 24;
+  if (numericBoost === -4) return 8 / 24;
   if (numericBoost === -3) return 0.4;
   if (numericBoost === -2) return 0.5;
-  if (numericBoost === -1) return 0.67;
+  if (numericBoost === -1) return 16 / 24;
   if (numericBoost === 1) return 1.5;
   if (numericBoost === 2) return 2;
   if (numericBoost === 3) return 2.5;
--- src/utils/pokemonConsts/ruins.ts
+++ src/utils/pokemonConsts/ruins.ts
@@ -1,7 +1,7 @@
 export const ruins = [
   { name: "common.none", value: "none" },
-  { name: "ruins.tablets", value: "break" },
-  { name: "ruins.sword", value: "fairy" },
-  { name: "ruins.vessel", value: "dark" },
-  { name: "ruins.beads", value: "dark" },
+  { name: "ruins.tablets", value: "tablets" },
+  { name: "ruins.sword", value: "sword" },
+  { name: "ruins.vessel", value: "vessel" },
+  { name: "ruins.beads", value: "beads" },
 ];
