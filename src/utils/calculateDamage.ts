931066473fe3b23cfb1592d039137f4f94dc1a0c
Ismael <iaarabu@gmail.com>
2023-04-03 00:05:31 +0100

    Fix type check bug and add set and extreme calc

--- src/utils/calculateDamage.ts
+++ src/utils/calculateDamage.ts
@@ -5,9 +5,17 @@ import {
   getCompleteDexNames,
 } from "./pokemonConsts/lists";
 import React, { SetStateAction } from "react";
-import { calculate, Generations } from "./calc";
+import { calculate, Generations, MOVES, SPECIES, TYPE_CHART } from "./calc";
 import { Pokemon } from "./calc/pokemon";
 import { Move } from "./calc/move";
+import { CalcData } from "../types/calcData";
+import { ItemName, NatureName } from "./calc/data/interface";
+
+type PokemonData = {
+  items: string;
+  nature: string;
+  evs: string;
+};
 
 export const loadDataCalculator = async (
   form: ReportProps,
@@ -21,35 +29,93 @@ export const loadDataCalculator = async (
 
   setTotalDex(dex.length);
   const calcsList = [];
+  const moveType = MOVES[MOVES.length - 1][form.move].type;
 
-  console.log(getCompleteDex());
   let i = 1;
   for (const pokemon of dex) {
+    if (i === 10) break;
     setNumberDex(i);
-    try {
-      const fetchData = await fetch(
-        `https://www.pikalytics.com/api/p/2023-02/gen9vgc2023series2-1760/${pokemon
-          .toLowerCase()
-          .trim()}`
-      );
-      const defensiveData = await fetchData.json();
-      console.log(defensiveData);
-      calculateDamage(form, pokemon, defensiveData);
-    } catch (e) {
-      console.log("Invalid data", pokemon, e);
-    } finally {
-      i++;
+    const pokemonType = SPECIES[SPECIES.length - 1][pokemon].types;
+    const typeValue = TYPE_CHART[TYPE_CHART.length - 1];
+
+    //1 - añadir inmunidad de tipos
+
+    if (
+      typeValue?.[moveType]?.[pokemonType[0]] === 0 ||
+      (pokemonType[1] && typeValue?.[moveType]?.[pokemonType[1]] === 0)
+    ) {
+      calcsList.push({
+        pokemon: pokemon,
+        isInmune: true,
+        description: "You can't hit it bro",
+        damage_range: 0,
+        percent_range: [0, 0],
+        ko_chance: { chance: 0, n: 0, text: "0%" },
+        defender_evs: { atk: 0, def: 0, spa: 0, spd: 0, spe: 0 },
+        text_evs: "0 Atk / 0 Def / 0 SpA / 0 SpD / 0 Spe",
+        move_category: MOVES[MOVES.length - 1][form.move].category,
+      });
+      continue;
     }
+
+    //PASOS:
+    //2 - el pokemon no tiene un set almacenado
+    const setCalc = calculateDamageWithSet(pokemon, form);
+    //3 - Añadir set Extremo
+    const extremeCalc = calculateExtremeDamage(pokemon, form);
+    //4 - Añadir set Óptimo
+
+    //////////////////////////////////////////
+    ///////                            ///////
+    ///////     OBTENCIÓN DE SET       ///////
+    ///////                            ///////
+    //////////////////////////////////////////
+
+    i++;
+  }
+};
+
+const calculateExtremeDamage = async (pokemon: any, form: ReportProps) => {
+  try {
+    const defensiveData: PokemonData = {
+      items: SPECIES[SPECIES.length - 1][pokemon].nfe ? "Eviolite" : "",
+      nature: form.category === "Physical" ? "Bold" : "Calm",
+      evs: "252/0/252/0/252/0",
+    };
+    return calculateDamage(form, pokemon, defensiveData);
+  } catch (e) {
+    console.log("Invalid data", pokemon, e);
+    return undefined;
+  }
+};
+
+const calculateDamageWithSet = async (pokemon: any, form: ReportProps) => {
+  try {
+    const fetchData = await fetch(
+      `https://www.pikalytics.com/api/p/2023-02/gen9vgc2023series2-1760/${pokemon
+        .toLowerCase()
+        .trim()}`
+    );
+    const responseData = await fetchData.json();
+    const defensiveData: PokemonData = {
+      items: responseData.items[0].item,
+      nature: responseData.spreads[0].nature,
+      evs: responseData.spreads[0].ev ?? "0/0/0/0/0/0",
+    };
+    return calculateDamage(form, pokemon, defensiveData);
+  } catch (e) {
+    console.log("Invalid data", pokemon, e);
+    return undefined;
   }
 };
 
 const calculateDamage = (
   form: ReportProps,
   pokemon: string,
-  defensiveData: any
+  defensiveData: PokemonData
 ) => {
-  const evSpread = getEvSpread(defensiveData.spreads[0].ev);
-  console.log(evSpread);
+  const evSpread = getEvSpread(defensiveData.evs);
+
   const result = calculate(
     9,
     new Pokemon(Generations.get(9), form.name, {
@@ -60,14 +126,15 @@ const calculateDamage = (
       boosts: { atk: +form.boostAtk, spa: +form.boostSpa },
     }),
     new Pokemon(Generations.get(9), pokemon, {
-      item: defensiveData.items[0].item,
-      nature: defensiveData.spreads[0].nature,
+      item: defensiveData.items as ItemName,
+      nature: defensiveData.nature as NatureName,
       evs: { hp: evSpread.hp, def: evSpread.def, spd: evSpread.spd },
       ivs: { hp: 31, def: 31, spd: 31 },
+      boosts: { def: +form.boostDef, spd: +form.boostSpd },
     }),
     new Move(Generations.get(9), form.move)
   );
-  const sendData = {
+  const sendData: CalcData = {
     description: result.desc(),
     damage_range: result.damage,
     percent_range: [
@@ -79,7 +146,7 @@ const calculateDamage = (
     text_evs: getEvsText(result.defender.evs),
     move_category: result.move.category,
   };
-  console.log(sendData);
+  return sendData;
 };
 
 const getEvSpread = (spread: string) => {
