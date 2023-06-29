27c300193241596e3308b7990e38140b47648b97
Ismael <iaarabu@gmail.com>
2023-06-29 21:11:00 +0100

    Change query pokemon name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pokemon-report-generator/",
+  base: "/pokemon-attack-analyzer/",
 });
