7127a9c1fe45397d8a29ab079fe6b0a762b186ae
Ismael Aarab <iaarabu@gmail.com>
2024-06-21 21:25:26 +0100

    feat: :sparkles: Add terapagos forms to the list

--- src/adapter/gen9.ts
+++ src/adapter/gen9.ts
@@ -3,15 +3,28 @@ import { MIN_EV_SPREAD } from "../utils/consts";
 import json from "../utils/gen9.json";
 
 const gen9Pokedex: Pokedex = json;
+const terapagosExtraForms = ["Terapagos-Stellar", "Terapagos-Terastal"];
 
 export const getGen9PokemonNames = () => {
-  return gen9Pokedex.map((pokemon) => pokemon.name);
+  const dex = gen9Pokedex.map((pokemon) => pokemon.name);
+  dex.push(...terapagosExtraForms);
+  return dex;
 };
 
 export const getGen9PokemonDefensiveDataList = () => {
   const list: Map<string, PokemonData> = new Map<string, PokemonData>();
   gen9Pokedex.forEach((pokemon) => {
     const { name, item, evs, nature, img } = pokemon;
+    if (name.toLowerCase() === "terapagos") {
+      terapagosExtraForms.forEach((form) => {
+        list.set(form, {
+          item: item ?? "",
+          evs: evs ?? MIN_EV_SPREAD,
+          nature: nature ?? "Hardy",
+        });
+      });
+    }
+
     list.set(name, {
       item: item ?? "",
       evs: evs ?? MIN_EV_SPREAD,
